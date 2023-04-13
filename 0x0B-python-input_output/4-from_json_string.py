bfee4384bf3a15dfff5eec911a05e408d49b989f
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-04-12 19:27:09 -0700

    task 4 is from JSON strng to object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,10 @@
-#!/usr/python3
-"""This module defines a JSON to object function"""
+#!/usr/bin/python3
+"""This module defines a JSON-to-object function"""
 
 
 import json
 
 
 def from_json_string(my_str):
-    """Returns the python object representation of a JSON string
-    """
+    """Returns the Python object representation of a JSON string"""
     return json.loads(my_str)
