489dac859c6c66bbd92e60fdf5c0b9879c1f22d4
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-04-12 18:21:14 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 """This module reads a txt file"""
 
 def read_file(filename=""):
+    """Prints the contents of the text file"""
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
