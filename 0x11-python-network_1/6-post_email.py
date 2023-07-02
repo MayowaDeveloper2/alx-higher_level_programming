728b87851e466d85b4cea0619e59e92af207819a
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-07-01 20:37:28 -0700

    all task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a POST request, display response"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
